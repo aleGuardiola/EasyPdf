2ccd84525bedb464e9349a818d382508c6baa6c3
Alejandro Guardiola <aleguardiolam@gmail.com>
2018-10-13 12:42:32 -0400

    Xaml Structuration

---
+++ EasyPdf/EasyPdf/EasyPdfDocument.cs
@@ -0,0 +1,23 @@
+﻿using EasyPdf.EasyPdfComponents;
+using Portable.Xaml;
+
+namespace EasyPdf
+{
+    public abstract class EasyPdfDocument
+    {
+        //Xaml code used to generate the pdf
+        string _xamlCode;
+        EDocument _document;
+
+        public EasyPdfDocument()
+        {
+            
+        }
+
+        protected void Initialize(string xamlCode)
+        {
+            _xamlCode = xamlCode;
+            _document = XamlServices.Load(xamlCode) as EDocument;                      
+        }
+    }
+}
---
+++ EasyPdf/EasyPdf/Helpers/TypeConverter.cs
@@ -0,0 +1,16 @@
+﻿using iText.Kernel.Pdf.Colorspace;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyPdf.Helpers
+{
+    internal static class TypeConverter
+    {
+        internal static iText.Kernel.Colors.Color ToITextColor(System.Drawing.Color color)
+        {
+            var dRGB = new iText.Kernel.Colors.DeviceRgb(color.R, color.G, color.B);
+            return iText.Kernel.Colors.Color.MakeColor( dRGB.GetColorSpace() );
+        }
+    }
+}
